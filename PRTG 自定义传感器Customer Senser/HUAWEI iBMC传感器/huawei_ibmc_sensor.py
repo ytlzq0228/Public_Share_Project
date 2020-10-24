a8dfd349a79717ad64c3b2df64d238d5c902f826
Doubi Lee <ytlzq0228@outlook.com>
2020-10-25 02:10:40 +0800

    更新至56个通道，实际测试PRTG可以承受超过单一传感器50个通道 更新紧急、严重告警状态逻辑

--- PRTG 自定义传感器Customer Senser/HUAWEI iBMC传感器/huawei_ibmc_sensor.py
+++ PRTG 自定义传感器Customer Senser/HUAWEI iBMC传感器/huawei_ibmc_sensor.py
@@ -57,9 +57,7 @@ def get_interest_sensor_value_list(ssh_shell,interest_sensor_name_list):
 		sensor_brief.remove(sensor_brief[len(sensor_brief)-1])
 		interest_sensor_value_list=[]
 		for i in sensor_brief:
-			#print(i)
 			sensor_name=i.split('|')[1].strip()
-			#print(sensor_name)
 			if sensor_name in interest_sensor_name_list:
 				interest_sensor_value_list.append(i)
 	
@@ -88,30 +86,19 @@ def printxml(interest_sensor_value_list):
 				print('       <mode>Absolute</mode>')
 				print('       <float>1</float>')
 				print('       <value>%s</value>'%i[2].strip())
-				if i[5].strip() != 'na' or i[10].strip() != 'na' or i[6].strip() != 'na' or i[9].strip() != 'na':
+				if i[6].strip() != 'na' or i[9].strip() != 'na' or i[7].strip() != 'na' or i[8].strip() != 'na':
 					print('       <LimitMode>1</LimitMode>')
-				if i[5] and i[5].strip() != 'na':
-					print('       <LimitMinError>%s</LimitMinError>'%i[5].strip())
-				if i[10] and i[10].strip() != 'na':
-					print('       <LimitMaxError>%s</LimitMaxError>'%i[10].strip())
 				if i[6] and i[6].strip() != 'na':
-					print('       <LimitMinWarning>%s</LimitMinWarning>'%i[6].strip())
+					print('       <LimitMinError>%s</LimitMinError>'%i[6].strip())
 				if i[9] and i[9].strip() != 'na':
-					print('       <LimitMaxWarning>%s</LimitMaxWarning>'%i[9].strip())
+					print('       <LimitMaxError>%s</LimitMaxError>'%i[9].strip())
+				if i[7] and i[7].strip() != 'na':
+					print('       <LimitMinWarning>%s</LimitMinWarning>'%i[7].strip())
+				if i[8] and i[8].strip() != 'na':
+					print('       <LimitMaxWarning>%s</LimitMaxWarning>'%i[8].strip())
 				if i[4] and i[4].strip() != 'ok':
 					print('       <Warning>1</Warning>')
 				print('   </result>')
-		#if link_state=='Active':
-		#	print('   <text>Link State: Active</text>')
-		#elif link_state=='No_Link_name':
-		#	print('   <text>Link State: No Link name</text>')
-		#	print('   <Warning>1</Warning>')
-		#elif link_state=='Busy':
-		#	print('   <text>Link State: Busy</text>')
-		#	print('   <Warning>1</Warning>')
-		#else:
-		#	print('   <text>Link State: %s</text>'%link_state)
-		#	print('   <Error>1</Error>')
 		print('</prtg>')
 	except Exception as err:
 		raise err
@@ -132,7 +119,7 @@ def print_warning_xml():
 			
 def main():
 	try:
-		interest_sensor_name_list=["Inlet Temp","Outlet Temp","PCH Temp","CPU1 Core Rem","CPU2 Core Rem","CPU1 DTS","CPU2 DTS","Cpu1 Margin","Cpu2 Margin","CPU1 MEM Temp","CPU2 MEM Temp","SYS 3.3V","SYS 5V","SYS 12V_1","SYS 12V_2","CPU1 DDR VPP1","CPU1 DDR VPP2","CPU2 DDR VPP1","CPU2 DDR VPP2","FAN1 Speed","FAN2 Speed","FAN3 Speed","FAN4 Speed","Power","Disks Temp","Power1","PS1 VIN","PS1 IIn","PS1 IOut","PS1 POut","PS1 Temp","PS1 Inlet Temp","Power2","PS2 VIN","PS2 IIn","PS2 IOut","PS2 POut","PS2 Temp","PS2 Inlet Temp","CPU1 VCore","CPU2 VCore","CPU1 DDR VDDQ","CPU1 DDR VDDQ2","CPU2 DDR VDDQ","CPU2 DDR VDDQ2","CPU1 VDDQ Temp","CPU2 VDDQ Temp","CPU1 VRD Temp","CPU2 VRD Temp"]
+		interest_sensor_name_list=["Inlet Temp","Outlet Temp","PCH Temp","CPU1 Core Rem","CPU2 Core Rem","CPU1 DTS","CPU2 DTS","Cpu1 Margin","Cpu2 Margin","CPU1 MEM Temp","CPU2 MEM Temp","SYS 3.3V","SYS 5V","SYS 12V_1","SYS 12V_2","CPU1 DDR VPP1","CPU1 DDR VPP2","CPU2 DDR VPP1","CPU2 DDR VPP2","FAN1 Speed","FAN2 Speed","FAN3 Speed","FAN4 Speed","Power","Disks Temp","Power1","PS1 VIN","PS1 IIn","PS1 IOut","PS1 POut","PS1 Temp","PS1 Inlet Temp","Power2","PS2 VIN","PS2 IIn","PS2 IOut","PS2 POut","PS2 Temp","PS2 Inlet Temp","CPU1 VCore","CPU2 VCore","CPU1 DDR VDDQ","CPU1 DDR VDDQ2","CPU2 DDR VDDQ","CPU2 DDR VDDQ2","CPU1 VDDQ Temp","CPU2 VDDQ Temp","CPU1 VRD Temp","CPU2 VRD Temp","CPU1 VSA","CPU2 VSA","CPU1 VCCIO","CPU2 VCCIO","PCH VPVNN","PCH PRIM 1V05"]
 		client.connect(deviceip,22,'Administrator','password',allow_agent=False,look_for_keys=False)
 		ssh_shell = client.invoke_shell()
 		interest_sensor_value_list=get_interest_sensor_value_list(ssh_shell,interest_sensor_name_list)
